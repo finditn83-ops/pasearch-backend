5fb1b249095137e657b44d8d7e88931c2da418aa
finditn83-ops <finditn83@gmail.com>
2025-10-14 12:40:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const DB_PATH = path.join(__dirname, "devices.db");
 const UPLOAD_DIR = path.join(__dirname, "uploads");
 const JWT_SECRET = process.env.JWT_SECRET || "dev_secret";
 const PORT = process.env.PORT || 5000;
-const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+const FRONTEND_URL = process.env.FRONTEND_URL || "pasearch-frontend.vercel.app";
 const ADMIN_EMAIL = "finditn83@gmail.com";
 
 // =====================
